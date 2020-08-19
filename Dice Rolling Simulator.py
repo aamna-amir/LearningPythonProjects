cc825786cff98005377c3051bfa99ff20caddede
Amna Amir <aamna48@outlook.com>
2020-08-20 02:03:40 +0500

    add Dice Rolling Simulator

---
+++ Dice Rolling Simulator.py
@@ -0,0 +1,9 @@
+# 4. Dice Rolling Simulator
+
+# As the name of the program suggests, we will be imitating a rolling dice. 
+# The program will generate a random number each dice the program runs, and the users can use the dice repeatedly for as long as he wants.  
+# When the user rolls the dice, the program will generate a random number between 1 and 6 (as on a standard dice).
+# The number will then be displayed to the user. 
+# It will also ask users if they would like to roll the dice again. 
+# The program should also include a function that can randomly grab a number within 1 to 6 and print it. 
+# This beginner-level project will help build a strong foundation for fundamental programming concepts.
