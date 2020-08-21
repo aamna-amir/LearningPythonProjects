fd71ca4df1627b927704b4b334994d806c576e86
Amna Amir <aamna48@outlook.com>
2020-08-21 23:19:17 +0500

    add Hangman.py file

---
+++ Hangman.py
@@ -0,0 +1,9 @@
+# 5. Hangman
+# This is more of a “guess the word” game. 
+# The core concepts you have to use while developing this project are variables, random, integer, strings, char, input and output, and boolean. 
+# In the game, users have to enter letter guesses, and each user will have a limited number of guesses 
+# (a counter variable is needed for limiting the guesses).
+# You can create a pre-organized list of words that users can grab words from. 
+# Also, you must include specific functions to check whether or not a user has entered a single letter or 
+# if the input letter is in the hidden word, to if the user has actually inputted a single letter, 
+# and to print the correct outcomes (letters).
