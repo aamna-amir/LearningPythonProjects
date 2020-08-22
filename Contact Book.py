87c9a0771715248dd86b01b5885a191d11cf1263
Amna Amir <aamna48@outlook.com>
2020-08-23 04:10:11 +0500

    add contact list and add_contact method

--- Contact Book.py
+++ Contact Book.py
@@ -7,6 +7,7 @@
 
 print("\nContact Book\n")
 
+contact_list = []
 class Contact:
     def __init__(self, name, address, phone, email):
         self.name = name
@@ -14,9 +15,29 @@ class Contact:
         self.phone = phone
         self.email = email
 
+    def add_contact(self):
+        print("\nAdd New Contact")
+        name = input("\nName: \t\t")
+        self.name = name
+        address = input("Address: \t")
+        self.address = address
+        phone = input("Phone No.: \t")
+        self.phone = phone
+        email = input("Email: \t\t")
+        self.email = email
+        contact_dict = {'Name': self.name, 'Address': self.address, 'Phone No': self.phone, 'Email': self.email}
+        contact_list.append(contact_dict)
+
     def display(self):
         print("\nContact Details\n")
-        print("Name: ", self.name)
-        print("Address: ", self.address)
-        print("Phone: ", self.phone)
-        print("Email: ", self.email)
+        print("Name: \t\t", self.name)
+        print("Address: \t", self.address)
+        print("Phone: \t\t", self.phone)
+        print("Email: \t\t", self.email)
+
+c1 = Contact('','','','')
+c1.display()
+c1.add_contact()
+c1.add_contact()
+print(contact_list)
+c1.display()
