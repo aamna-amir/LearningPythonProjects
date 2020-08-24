dc3c5e4ce5c6edd7e494420dfccd0bab32cd2e86
Amna Amir <aamna48@outlook.com>
2020-08-24 23:47:55 +0500

    add Email Slicer.py file

---
+++ Email Slicer.py
@@ -0,0 +1,5 @@
+# 7. Email Slicer
+# This is a convenient program that has a lot of use in the future. 
+# The program helps get you the username and domain name from an email address. 
+# You can even customize the application and send a message to the host with this information.
+
