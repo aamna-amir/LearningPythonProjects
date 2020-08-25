b1d35763d667419ee2ff7b37e6324329511c1e58
Amna Amir <aamna48@outlook.com>
2020-08-25 23:36:20 +0500

    add get_username and get_domain functions

--- Email Slicer.py
+++ Email Slicer.py
@@ -3,3 +3,8 @@
 # The program helps get you the username and domain name from an email address. 
 # You can even customize the application and send a message to the host with this information.
 
+def get_username():
+    pass
+
+def get_domain():
+    pass
